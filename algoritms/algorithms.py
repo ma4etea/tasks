c6c23649a932e9c053ea5d4682a88decee2a8d7c
ma4etea <alexboradi@gmail.com>
2025-11-20 18:47:23 +0300

    D

--- algoritms/algorithms.py
+++ algoritms/algorithms.py
@@ -47,3 +47,38 @@ def two_sum(self, nums: list[int], target: int) -> list[int]:
             return [table_map[diff], index]
 
         table_map[num] = index
+
+def longest_palindrome(self, s: str) -> int:
+    """Самый длинный полиндром"""
+
+    letter_quantity_map = {}
+
+    for letter in s:
+        letter_quantity_map[letter] = letter_quantity_map.get(letter, 0) + 1
+
+    result = 0
+    center_used = False
+    for quantity in letter_quantity_map.values():
+        result += quantity // 2 * 2 #
+        if quantity % 2 == 1 and not center_used:
+            center_used = True
+
+    return result + center_used
+
+
+def insert_search(nums: list[int], target: int) -> int:
+    left_index = 0
+    right_index = len(nums) - 1
+
+    # todo бинарный поиск
+    while left_index <= right_index:
+        mid_index = (right_index - left_index) // 2 + left_index
+
+        if nums[mid_index] == target:
+            return mid_index
+        elif target > nums[mid_index]:
+            left_index = mid_index + 1
+        elif target < nums[mid_index]:
+            right_index = mid_index - 1
+
+    return left_index
